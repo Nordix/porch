02aa89f57aaf2c6d62656458f13617127b9f865c
liamfallon <liam.fallon@est.tech>
2025-04-07 13:25:07 +0100

    Rebase against meta-move

--- pkg/externalrepo/fake/packagerevision.go
+++ pkg/externalrepo/fake/packagerevision.go
@@ -100,3 +100,7 @@ func (f *FakePackageRevision) GetMeta() metav1.ObjectMeta {
 func (f *FakePackageRevision) SetMeta(context.Context, metav1.ObjectMeta) error {
 	return nil
 }
+
+func (f *FakePackageRevision) UpdateResources(ctx context.Context, new *v1alpha1.PackageRevisionResources, change *v1alpha1.Task) error {
+	return nil
+}
