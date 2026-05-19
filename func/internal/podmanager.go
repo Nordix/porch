acbdc6f0c8ed0ec45e7c7aff6d039185349bdaf7
Catalin-Stratulat-Ericsson <catalin.stratulat@ericsson.com>
2026-05-19 10:36:15 +0100

    comment removed to retrigger test
    Signed-off-by: Catalin-Stratulat-Ericsson <catalin.stratulat@ericsson.com>

--- func/internal/podmanager.go
+++ func/internal/podmanager.go
@@ -891,7 +891,6 @@ func podID(image, hash, postFix string) (string, error) {
 		return "", fmt.Errorf("unable to parse image reference %v: %w", image, err)
 	}
 
-	// repoName will be something like ghcr.io/kptdev/krm-functions-catalog/set-namespace
 	repoName := ref.Context().Name()
 	tag := strings.ReplaceAll(ref.Identifier(), ".", "")
 	parts := strings.Split(repoName, "/")
